8d9268a81e6e2c526e2bb635eded086575ebac3e
peerapattop <peerapatza401@gmail.com>
2024-09-24 19:55:50 +0700

    update

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
-import 'expenses_list.dart';
-import 'models/expense.dart';
+import 'package:expense_tracker/models/expense.dart';
+
+import 'expenses_list/expenses_list.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
 
   @override
-  State<Expenses> createState() => _ExpensesState();
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
 }
 
 class _ExpensesState extends State<Expenses> {
@@ -17,7 +20,13 @@ class _ExpensesState extends State<Expenses> {
       amount: 19.99,
       date: DateTime.now(),
       category: Category.work,
-    )
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
   ];
 
   @override
@@ -25,11 +34,9 @@ class _ExpensesState extends State<Expenses> {
     return Scaffold(
       body: Column(
         children: [
-          const Text('data'),
+          const Text('The chart'),
           Expanded(
-            child: ExpensesList(
-              expenses: _registeredExpenses,
-            ),
+            child: ExpensesList(expenses: _registeredExpenses),
           ),
         ],
       ),
---
+++ lib/widgets/expenses_list/expenses_item.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../../models/expense.dart';
+
+class ExpensesItem extends StatelessWidget {
+  const ExpensesItem(this.expenses, {super.key});
+
+  final Expense expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 20,
+          horizontal: 16,
+        ),
+        child: Column(
+          children: [
+            Text(expenses.title),
+            const SizedBox(height: 4),
+            Row(
+              children: [
+                Text('\$ ${expenses.amount.toStringAsFixed(2)}'),
+                const Spacer(),
+                Row(
+                  children: [
+                    const Icon(Icons.alarm),
+                    const SizedBox(width: 8),
+                    Text(expenses.date.toString()),
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
